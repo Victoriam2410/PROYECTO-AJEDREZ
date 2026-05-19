1dbd60357b0e055acee200856dd27ec846e3af09
Victoriam2410 <estherrodas61@gmail.com>
2026-05-18 22:54:43 -0600

    Usuario y contraseña
    Aqui ya queme la contraseña y el usuario

---
+++ Login.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Windows.Forms;
+
+namespace AjedrezJuego
+{
+    public class FormLogin : Form
+    {
+        private Label lblTitulo;
+        private Label lblUsuario;
+        private Label lblContrasena;
+        private TextBox txtUsuario;
+        private TextBox txtContrasena;
+
+
+        private const string USUARIO_CORRECTO = "admin";
+        private const string CONTRASENA_CORRECTA = "Juego@123";
+
+        }
+    }
+}
