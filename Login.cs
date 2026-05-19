a4c13ef4a074e40ee2dfb195a0b1756fd45d1e57
Victoriam2410 <estherrodas61@gmail.com>
2026-05-18 22:57:26 -0600

    Titulos
    Cree el titulo del formulario y el titulo del juego

--- Login.cs
+++ Login.cs
@@ -14,6 +14,21 @@ namespace AjedrezJuego
 
         private const string USUARIO_CORRECTO = "admin";
         private const string CONTRASENA_CORRECTA = "Juego@123";
+        public FormLogin()
+        {
+            this.Text = "JUEGO DE AJEDREZ - Login";
+            this.Size = new System.Drawing.Size(400, 320);
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.BackColor = System.Drawing.Color.LightYellow;
+
+            lblTitulo = new Label();
+            lblTitulo.Text = "===== JUEGO DE AJEDREZ =====";
+            lblTitulo.Font = new System.Drawing.Font("Arial", 13, System.Drawing.FontStyle.Bold);
+            lblTitulo.Location = new System.Drawing.Point(50, 20);
+            lblTitulo.Size = new System.Drawing.Size(300, 30);
+            lblTitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 
         }
     }
